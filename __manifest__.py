b5ea11d46ed1e34596b3a1a142778ad4b5f7d4b4
BintangNur <bintangnur46@gmail.com>
2023-02-21 10:40:11 +0700

    upload

---
+++ __manifest__.py
@@ -0,0 +1,27 @@
+{
+    "name" : "New Addons",
+    "version" : "1.0",
+    "author" : "Bintang Nur Auliya",
+    "website" : "https://www.detik.com/",
+    "category" : "New Module",
+    "summary" : "Ini adalah oddons baru",
+    "discription" : """
+        Latihan membuat addons baru di odoo 1
+        """,
+    "depends" : [
+        "base",
+    ],
+    "data" : [
+
+    ],
+    "demo" : [],
+    "test" : [],
+    "image" : [],
+    "qweb" :[],
+    "css" : [],
+    "application" : True,
+    "installable" : True,
+    "auto_install" : False,
+
+    
+}
